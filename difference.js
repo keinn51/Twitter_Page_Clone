9ec091359ac3b142cd46ce0fd66de7dfeae6bd7f
keinn51 <soogood97@gmail.com>
2022-02-08 15:39:06 +0900

    Implement image loader part two (v4.1)
    Just get the data using "readAsDataURL".

--- difference.js
+++ difference.js
@@ -5,10 +5,22 @@ const onFileChange = (event) => {
     const theFile = files[0];
     const reader = new FileReader();
     reader.onloadend = (finishedEvent) => {
-        console.log(finishedEvent);
+        const {
+            currentTarget: { result },
+        } = finishedEvent;
+        console.log(finishedEvent.currentTarget)
+        setAttachment(result);
     };
     reader.readAsDataURL(theFile);
 };
 
 <input type="file" accept="image/*" onChange={onFileChange} />
-
+    <input type="submit" value="Nweet" />
+{
+    attachment && (
+        <div>
+            <img src={attachment} width="100px" height="100px" />
+            <button onClick={onClearAttachment}>Clear</button>
+        </div>
+    )
+}
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -12,6 +12,7 @@ import Nweet from "components/Nweet";
 const Home = ({ userObj }) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
+    const [attachment, setAttachment] = useState();
 
     const onSubmit = async (event) => {
         event.preventDefault();
@@ -41,10 +42,16 @@ const Home = ({ userObj }) => {
         const theFile = files[0];
         const reader = new FileReader();
         reader.onloadend = (finishedEvent) => {
-            console.log(finishedEvent);
+            const {
+                currentTarget: { result },
+            } = finishedEvent;
+            console.log(finishedEvent.currentTarget)
+            setAttachment(result);
         };
         reader.readAsDataURL(theFile);
     };
+
+    const onClearAttachment = () => setAttachment(null);
 
     useEffect(() => {
         const q = query(
@@ -72,6 +79,12 @@ const Home = ({ userObj }) => {
                 />
                 <input type="file" accept="image/*" onChange={onFileChange} />
                 <input type="submit" value="Nweet" />
+                {attachment && (
+                    <div>
+                        <img src={attachment} width="100px" height="100px" />
+                        <button onClick={onClearAttachment}>Clear</button>
+                    </div>
+                )}
             </form>
             <div>
                 {nweets.map((nweet) => (

b2b813be59e39dcf65ccb0e2d49d1a11ddbcae54
keinn51 <soogood97@gmail.com>
2022-02-09 10:04:54 +0900

    Implement publishing my nweet project (v6.2)
    - What I do
    I used gh-pages to publsih my project in github envorinment.
    I set up the package.json and use "deploy" option.
    Also add the "hompage" key to send my builded java script code.

--- difference.js
+++
@@ -1,8 +0,0 @@
-const refreshUser = () => {
-    const user = authService.currentUser;
-    setUserObj({
-        displayName: user.displayName,
-        uid: user.uid,
-        updateProfile: () => updateProfile(user, { displayName: user.displayName }),
-    });
-};
